f8700f6b8b235e4dae626ea8d7041804d96cde07
giosantoro-meli <giovanni.santoro@mercadolivre.com>
2021-06-10 16:46:44 -0300

    first commit

---
+++ src/main/java/com/mercadolivre/bootcamp/QualityChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QualityChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mercadolivre/bootcamp/QualityChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
